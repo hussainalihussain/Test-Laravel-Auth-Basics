ded8d397febe7943b4200aac646a0b970907a6c9
hussainali <hussainali9616@gmail.com>
2024-05-06 04:06:14 +0500

    added min:8 to the rule

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', 'regex:/^[a-zA-Z].*/'],
+            'password' => ['required', 'confirmed', 'min:8', 'regex:/^[a-zA-Z].*/'],
         ]);
 
         $user = User::create([
